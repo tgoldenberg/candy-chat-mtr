3b64ca344629b3dcc7370bc507a8b47b54354e57
tgoldenberg <thomasagoldenberg@gmail.com>
2015-10-27 22:15:07 -0400

    configure for DDP connection with Android app

--- server/publications.js
+++ server/publications.js
@@ -1,9 +1,10 @@
-Meteor.publish('messages', function(options) {
-  return Messages.find({}, {limit: 100});
+Meteor.publish('messages', function() {
+  console.log('Publishing Messages', Messages.find().count());
+  return Messages.find();
 });
-Meteor.publish('emojis', function(options) {
+Meteor.publish('emojis', function() {
   return Emojis.find({}, {limit: 100});
 });
-Meteor.publish('photos', function(options) {
+Meteor.publish('photos', function() {
   return Photos.find({}, {limit: 100});
 });
